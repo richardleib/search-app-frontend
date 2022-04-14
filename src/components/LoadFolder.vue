d499fc8f189360451ceaafaf6404227896c4b3e4
Richard Leibowitz <richardbbleibowitz@gmail.com>
2022-04-15 01:32:29 +0300

    Update css remove unused

--- src/components/LoadFolder.vue
+++ src/components/LoadFolder.vue
@@ -24,7 +24,7 @@
           </v-carousel>
         </v-col>
       </v-row>
-      <v-row class="ma-6 pa-6">
+      <v-row class="ma-2 pa-2">
         <v-col cols="12">
           <FilesList>
             <template #item="{ id, itemId, dataUrl }">
