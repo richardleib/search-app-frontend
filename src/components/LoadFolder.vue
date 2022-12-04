72bac8e3377ccf19972581c6ee23bfd1560bfe5e
Richard Leibowitz <richardbbleibowitz@gmail.com>
2022-12-05 00:26:28 +0300

    Fix images position in page

--- src/components/LoadFolder.vue
+++ src/components/LoadFolder.vue
@@ -1,6 +1,15 @@
 <template>
   <v-card class="container">
     <v-container fluid>
+      <v-row>
+        <v-col cols="12">
+          <v-breadcrumbs :items="breadcrumbs">
+            <template v-slot:divider>
+              <v-icon>mdi-chevron-right</v-icon>
+            </template>
+          </v-breadcrumbs>
+        </v-col>
+      </v-row>
       <v-row v-if="hasImages">
         <v-col cols="12">
           <v-carousel
@@ -14,15 +23,6 @@
               :src="'https://link12.ddns.net:9090' + image.thumbUrl">
             </v-carousel-item>
           </v-carousel>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col cols="12">
-          <v-breadcrumbs :items="breadcrumbs">
-            <template v-slot:divider>
-              <v-icon>mdi-chevron-right</v-icon>
-            </template>
-          </v-breadcrumbs>
         </v-col>
       </v-row>
       <v-row>
